8b5da2ceee49abf73f224ba00498eaaed096b3b1
funhappy-all <hansang6082@unist.ac.kr>
2025-03-04 21:26:45 +0900

    문제 풀이

--- 11022.py
+++ 11022.py
@@ -4,9 +4,9 @@ ca = []
 T = int(input())
 for i in range(0,T):
     a, b = map(int, input().split())
-    a.append(a)
-
+    al.append(a)
+    bl.append(b)
     ca.append(a+b)
 
 for j in range(0,T):
-    print("Case #" + str(j+1) + ": " +str(ca[j]))
+    print("Case #" + str(j+1) + ": " +str(al[j])+ " + "+str(bl[j])+" = "+str(ca[j]))
